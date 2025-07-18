f5d1e1cead9f60355b6445ad6c994c991b069e59
flpflan <flpflan@gmail.com>
2025-07-18 16:15:26 +0800

    adopt official flake

--- nix/home/flpflan/common/desktop/shell/quickshell/caelestia/default.nix
+++ nix/home/flpflan/common/desktop/shell/quickshell/caelestia/default.nix
@@ -1,20 +1,20 @@
-{ config, lib, pkgs, quickshell, ... }:
+{ 
+  config,
+  lib,
+  pkgs,
+  caelestia-shell,
+  ...
+}:
 
 let
-  qs-caelestia = pkgs.callPackage ./package.nix { inherit quickshell; };
+  # qs-caelestia = pkgs.callPackage ./package.nix { inherit quickshell; };
+  qs-caelestia = caelestia-shell.packages."${pkgs.system}".default.override { withCli = true; };
 in
 {
-  home.packages = [ qs-caelestia ];
-  xdg.configFile."quickshell/caelestia" = {
-    source = pkgs.fetchFromGitHub {
-      owner = "liperium";
-      repo = "caelestia-shell";
-      rev = "ef5f5b6d35b03b5e887857df872c02308bcb2cbc";
-      sha256 = "sha256-GHaUs6FXI7lZLpT+VENGe9VyqUdzTjA7FPgGG2A4AXU=";
-    };
-    recursive = true;
-  };
+  home.packages = [
+    qs-caelestia
+  ];
   wayland.windowManager.hyprland.settings.exec-once = lib.mkIf config.wayland.windowManager.hyprland.enable [
-    "${qs-caelestia}/bin/qs"
+    "${qs-caelestia}/bin/caelestia-shell"
   ];
 }
--- nix/home/flpflan/common/desktop/shell/quickshell/caelestia/package.nix
+++ nix/home/flpflan/common/desktop/shell/quickshell/caelestia/package.nix
@@ -1,6 +1,13 @@
-{ lib, pkgs, quickshell, ... }:
+{
+  pkgs,
+  quickshell,
+  fetchFromGitHub,
+  ...
+}:
 
 let
+  shortRev = rev: builtins.substring 0 7 rev;
+
   qs = quickshell.packages."${pkgs.system}".default.override {
     withJemalloc = true;
     withQtSvg = true;
@@ -12,36 +19,98 @@ let
     withI3 = false;
   };
   deps = with pkgs; [
-    # Cmdline tools
-    git curl jq ibm-plex fd fish 
-    cava ddcutil brightnessctl imagemagick bluez networkmanager
-    # Python dependencies
-    (
-      python3.withPackages (ps: with ps; [
-        aubio pyaudio numpy
-      ])
-    )
-    # app2unit
+    # TODO:
+    app2unit
+    aubio
+    # bluez
+    brightnessctl
+    cava
+    # curl
+    ddcutil
+    fishMinimal
+    grim
+    inotify-tools
+    libqalculate
+    lm_sensors
+    # material-symbols
+    # nerd-fonts.jetbrains-mono
+    networkmanager
+    pipewire
+    # power-profiles-daemon
+    # procps
+    kdePackages.qtdeclarative
+    qs
+    swappy
   ];
   fontconfig = pkgs.makeFontsConf {
     fontDirectories = with pkgs; [
       material-symbols nerd-fonts.jetbrains-mono
     ];
   };
-  qs-wrapped = pkgs.symlinkJoin {
-    name = "quickshell-caelestia";
-    paths = [ qs ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/qs \
-      --set FONTCONFIG_FILE "${fontconfig}" \
-      --add-flags "-c caelestia"
-    '';
-
-    meta.mainProgram = "quickshell";
-  };
 in
-pkgs.writeShellScriptBin "qs" ''
-  export PATH=${lib.makeBinPath deps}:$PATH
-  exec ${qs-wrapped}/bin/qs "$@"
-''
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "caelestia-shell";
+  src = fetchFromGitHub {
+    # owner = "caelestia-dots";
+    # repo = "shell";
+    # rev = "ad22b1e7e6067bc9a0304048e7bf2f712769f4f5";
+    # sha256 = "sha256-3xL8yg61J5w4o1zkunTYxqlb1C+BAt7uaZJLNZlOkRQ=";
+    owner = "pinksteven";
+    repo = "caelestia-shell";
+    rev = "48922fc9273a7d8165a26d8b7aff33e6e764218c";
+    sha256 = "sha256-z2AKOtFH57Q7/IlpzRdideYdr1pF8pakqD+x5E7HRFM=";
+  };
+  # version = self.shortRev or "dirty";
+  version = shortRev src.rev;
+
+  nativeBuildInputs = with pkgs; [
+    gcc
+    makeWrapper
+  ];
+
+  buildInputs = deps;
+  dontWrapQtApps = true;
+
+  buildPhase = ''
+    mkdir -p bin
+    g++ -std=c++17 -Wall -Wextra \
+    -I${pkgs.pipewire.dev}/include/pipewire-0.3 \
+    -I${pkgs.pipewire.dev}/include/spa-0.2 \
+    -I${pkgs.aubio}/include/aubio \
+    assets/beat_detector.cpp \
+    -o bin/beat_detector \
+    -lpipewire-0.3 -laubio
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/share/quickshell
+    mkdir -p $out/usr/lib/caelestia
+
+    cp -r assets config modules services utils widgets $out/share/quickshell/
+    cp shell.qml $out/share/quickshell/
+    cp bin/beat_detector $out/usr/lib/caelestia
+    mv run.fish $out/bin/caelestia-shell
+    chmod +x $out/bin/caelestia-shell
+
+    # Passing entire qs command via the script
+    sed -i "/qs/i \
+    if count \$argv > \/dev\/null \
+    \n  qs -p $out\/share\/quickshell \$argv \
+    \nelse" $out/bin/caelestia-shell
+
+    sed -i "/\$cache/a end" $out/bin/caelestia-shell
+
+    substituteInPlace $out/bin/caelestia-shell --replace-quiet "qs -p (dirname (status filename))" "  qs -p $out/share/quickshell"
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/caelestia-shell \
+    --set CAELESTIA_BD_PATH "$out/usr/lib/caelestia/beat_detector" \
+    --prefix PATH : ${pkgs.lib.makeBinPath deps} \
+    --set FONTCONFIG_FILE "${fontconfig}"
+    # --prefix FONTCONFIG_PATH : ${pkgs.fontconfig}/etc/fonts
+    # --set QT_QUICK_CONTROLS_STYLE Basic
+  '';
+}
