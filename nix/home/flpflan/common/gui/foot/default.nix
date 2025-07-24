8515cc0424076f91f4c3d342f46438c2d02956c5
flpflan <flpflan@gmail.com>
2025-07-24 16:20:42 +0800

    fix foot

--- nix/home/flpflan/common/gui/foot/default.nix
+++ nix/home/flpflan/common/gui/foot/default.nix
@@ -1,14 +1,13 @@
 { config, pkgs, fl-dots, ... }:
 
 let
-  isEmpty = attrs: builtins.length (builtins.attrNames attrs) == 0;
   outOfStore = config.lib.file.mkOutOfStoreSymlink;
 
   wrapedConfig = pkgs.writeText "foot.ini" (builtins.concatStringsSep "\n" [
     "include = ${outOfStore "${fl-dots}/foot/foot.ini"}"
     (pkgs.lib.optionalString
-      (isEmpty config.programs.foot.settings)
-      # "include = ${config.xdg.configHome}/foot/foot.ini"
+      (config.programs.foot.settings != {})
+      "include = ${config.xdg.configHome}/foot/foot.ini"
     )
   ]);
 
