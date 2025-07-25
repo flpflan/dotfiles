9f891217b83d5d757e865cb6793bd8c131027723
flpflan <flpflan@gmail.com>
2025-07-25 18:42:48 +0800

    tweak fcitx5

--- nix/home/flpflan/common/gui/fcitx5/default.nix
+++ nix/home/flpflan/common/gui/fcitx5/default.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   i18n.inputMethod = {
@@ -7,25 +7,29 @@
     fcitx5 = {
       waylandFrontend = true;
       addons =  with pkgs; [
+        # 插件/引擎
         # fcitx5-gtk # Needed in x11
         fcitx5-mozc
         # fcitx5-chinese-addons # We use rime instead of this
-
-        fcitx5-material-color
-
-        rime-zhwiki
-        nur.repos.xyenon.rime-moegirl
-
         # fcitx5-rime.override {
         #   rimeDataPkgs = [ nur.repos.xyenon.rime-ice ];
         # }
         fcitx5-rime
+
+        # 词库
+        rime-zhwiki
+        nur.repos.xyenon.rime-moegirl
         nur.repos.xyenon.rime-ice
+
+        # 主題
+        fcitx5-fluent
+        fcitx5-mellow-themes
       ];
       settings = {
         addons = {
-          classicui.globalSection.Theme = lib.mkDefault "Material-Color-deepPurple";
-          classicui.globalSection.DarkTheme = lib.mkDefault "Material-Color-deepPurple";
+          classicui.globalSection.Theme = "FluentLight";
+          classicui.globalSection.DarkTheme = "FluentDark";
+          classicui.globalSection.UseDarkTheme = false; # 跟随系统浅色/深色设置 
           # pinyin.globalSection = {
           #   PageSize = 9;
           #   CloudPinyinEnabled = "True";
