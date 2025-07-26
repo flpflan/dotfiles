07bb2e235b9053150a232d94b6312d4388b457e7
flpflan <flpflan@gmail.com>
2025-07-26 21:11:39 +0800

    meh

--- nix/home/flpflan/fl-pc/style/manual/gtk.nix
+++ nix/home/flpflan/fl-pc/style/manual/gtk.nix
@@ -1,7 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, fl-dots, ... }:
 
 {
   home.packages = with pkgs; [
     adw-gtk3 papirus-icon-theme
   ];
+
+  xdg.configFile."gtk-4.0/settings.ini" = "${fl-dots}/gtk-4.0/settings.ini";
 }
