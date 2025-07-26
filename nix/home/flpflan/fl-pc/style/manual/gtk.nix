0618a08da7ffed84cd442ddad3b5dbdf62ef4a2e
flpflan <flpflan@gmail.com>
2025-07-26 21:19:41 +0800

    meh

--- nix/home/flpflan/fl-pc/style/manual/gtk.nix
+++ nix/home/flpflan/fl-pc/style/manual/gtk.nix
@@ -1,10 +1,13 @@
-{ pkgs, fl-dots, ... }:
+{ config, pkgs, fl-dots, ... }:
 
+let
+  outOfStore = config.lib.file.mkOutOfStoreSymlink;
+in 
 {
   home.packages = with pkgs; [
     adw-gtk3 papirus-icon-theme
   ];
 
-  xdg.configFile."gtk-4.0/settings.ini".source = "${fl-dots}/gtk-4.0/settings.ini";
-  xdg.configFile."gtk-3.0/settings.ini".source = "${fl-dots}/gtk-3.0/settings.ini";
+  xdg.configFile."gtk-4.0/settings.ini".source = outOfStore "${fl-dots}/gtk-4.0/settings.ini";
+  xdg.configFile."gtk-3.0/settings.ini".source = outOfStore "${fl-dots}/gtk-3.0/settings.ini";
 }
