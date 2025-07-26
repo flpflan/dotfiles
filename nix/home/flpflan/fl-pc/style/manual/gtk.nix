aba0fa26fe5b3e7ef6212e0b5f5d9be97c39c42c
flpflan <flpflan@gmail.com>
2025-07-26 21:15:07 +0800

    meh

--- nix/home/flpflan/fl-pc/style/manual/gtk.nix
+++ nix/home/flpflan/fl-pc/style/manual/gtk.nix
@@ -5,6 +5,6 @@
     adw-gtk3 papirus-icon-theme
   ];
 
-  xdg.configFile."gtk-4.0/settings.ini" = "${fl-dots}/gtk-4.0/settings.ini";
-  xdg.configFile."gtk-3.0/settings.ini" = "${fl-dots}/gtk-3.0/settings.ini";
+  xdg.configFile."gtk-4.0/settings.ini".source = "${fl-dots}/gtk-4.0/settings.ini";
+  xdg.configFile."gtk-3.0/settings.ini".source = "${fl-dots}/gtk-3.0/settings.ini";
 }
