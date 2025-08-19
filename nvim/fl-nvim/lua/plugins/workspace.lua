a2ee323e2651d4c542f98c9aaf461bf6b81ed7eb
flpflan <flpflan@gmail.com>
2025-08-19 17:38:13 +0800

    feature(nvim): workspace

---
+++ nvim/fl-nvim/lua/plugins/workspace.lua
@@ -0,0 +1,21 @@
+plugin("exrc")
+  :event_defer()
+  :init(function() vim.g.workspace_file_name = ".nvim.lua" end)
+  :opts({
+    on_vim_enter = false,
+    min_log_level = vim.log.levels.INFO,
+  })
+  :setup(function()
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      pattern = { "*/" .. vim.g.workspace_file_name },
+      callback = function()
+        vim.defer_fn(function() vim.cmd "ExrcReloadAll" end, 100)
+      end,
+    })
+    require("exrc.loader").on_vim_enter()
+  end)
+
+require("project_nvim").setup {
+  patterns = { ".git", ".nvim.lua", ".svn", "Makefile", "package.json" },
+  ignore_lsp = {},
+}
