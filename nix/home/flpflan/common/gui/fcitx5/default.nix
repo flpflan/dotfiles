a2527db75cb654a972eaacfc33579ad49599ab7e
flpflan <flpflan@gmail.com>
2025-08-19 21:30:31 +0800

    fcitx5: tweak settings

--- nix/home/flpflan/common/gui/fcitx5/default.nix
+++ nix/home/flpflan/common/gui/fcitx5/default.nix
@@ -1,25 +1,27 @@
-{ config, pkgs, fl-dots, ... }:
-
-let
-  outOfStore = config.lib.file.mkOutOfStoreSymlink;
-in
 {
+  config,
+  pkgs,
+  fl-dots,
+  ...
+}: let
+  outOfStore = config.lib.file.mkOutOfStoreSymlink;
+in {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5 = {
       waylandFrontend = true;
-      addons =  with pkgs; [
+      addons = with pkgs; [
         # 插件/引擎
         # fcitx5-gtk # Needed in x11
         fcitx5-mozc
         # fcitx5-chinese-addons # We use rime instead of this
         (fcitx5-rime.override {
           rimeDataPkgs = [
-              # 词库
-              rime-ice
-              rime-zhwiki
-              rime-moegirl
+            # 词库
+            rime-ice
+            rime-zhwiki
+            rime-moegirl
           ];
         })
 
@@ -28,10 +30,17 @@ in
         fcitx5-mellow-themes
       ];
       settings = {
+        globalOptions = {
+          Hotkey = {
+            TriggerKeys = "";
+          };
+        };
         addons = {
           classicui.globalSection.Theme = "FluentDark";
           classicui.globalSection.DarkTheme = "FluentDark";
-          classicui.globalSection.UseDarkTheme = true; # 跟随系统浅色/深色设置 
+          classicui.globalSection.UseDarkTheme = true; # 跟随系统浅色/深色设置
+
+          clipboard.sections = {"TriggerKey" = "";};
         };
         inputMethod = {
           "Groups/0" = {
