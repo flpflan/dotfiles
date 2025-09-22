64eaea20a329030dad03da673c89bf4f9342035f
flpflan <flpflan@gmail.com>
2025-09-22 14:21:39 +0800

    service: udisks

---
+++ nix/hosts/fl-pc/system/mechanism/services/udisk.nix
@@ -0,0 +1,19 @@
+{
+  services.udisks2 = {
+    enable = true;
+    settings = {
+      "udisks2.conf" = {
+        defaults = {
+          encryption = "luks2";
+          btrfs_defaults = "rw,noatime,compress=zstd";
+        };
+        udisks2 = {
+          modules = [
+            "*"
+          ];
+          modules_load_preference = "ondemand";
+        };
+      };
+    };
+  };
+}
