345c8574c4ab0b5e05edb74240740d2445c2c47e
flpflan <flpflan@gmail.com>
2025-11-03 17:18:30 +0800

    meh

--- nix/home/flpflan/fl-pc/profile.nix
+++ nix/home/flpflan/fl-pc/profile.nix
@@ -1,5 +1,11 @@
-{fl-dots, ...}: {
+{
+  config,
+  fl-dots,
+  ...
+}: let
+  outOfStore = config.lib.file.mkOutOfStoreSymlink;
+in {
   home.file = {
-    "Pictures/wallpaper".source = "${fl-dots}/wallpaper";
+    "Pictures/wallpaper".source = outOfStore "${fl-dots}/wallpaper";
   };
 }
