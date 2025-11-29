dc94a60bf80a7d0d5ba9f41b401d8e6207f5a138
flpflan <flpflan@gmail.com>
2025-11-29 19:27:13 +0800

    imv: some fixes

--- nix/home/flpflan/common/QoL/imv/default.nix
+++ nix/home/flpflan/common/QoL/imv/default.nix
@@ -5,10 +5,19 @@ let
   outOfStore = config.lib.file.mkOutOfStoreSymlink;
 in 
 {
-  home.packages = [ pkgs.imv ];
+  home.packages = [
+    # (pkgs.imv.override {
+    #   withWindowSystem = "wayland"; # FIXME: cannot build
+    # })
+    pkgs.imv
+  ];
 
   xdg.configFile."imv" = {
     source = outOfStore "${dots}/imv";
     recursive = false;
+  };
+  xdg.dataFile = {
+    "applications/imv.desktop".source = ./imv.desktop;
+    "applications/imv-dir.desktop".source = ./imv-dir.desktop;
   };
 }
