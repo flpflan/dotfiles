0d47f041861f265056f84770a29ca17a0d7395c7
flpflan <flpflan@gmail.com>
2026-01-27 18:22:47 +0800

    nvim: diagnostic signs

---
+++ nvim/fl-nvim/lua/plugins/diagnostics/signs.lua
@@ -0,0 +1,15 @@
+vim.diagnostic.config {
+  virtual_text = true,
+  update_in_insert = true,
+  signs = {
+    text = {
+      -- [vim.diagnostic.severity.ERROR] = "",
+      -- [vim.diagnostic.severity.WARN] = "",
+      -- [vim.diagnostic.severity.INFO] = "",
+      -- [vim.diagnostic.severity.HINT] = "",
+    },
+  },
+  underline = true,
+  severity_sort = true,
+  -- float = {},
+}
