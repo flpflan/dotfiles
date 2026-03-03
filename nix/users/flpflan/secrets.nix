42820c9f1c044752f42cc255cc0d4d94fb7377cf
flpflan <flpflan@gmail.com>
2026-03-03 17:57:26 +0800

    nix/secrets: add extra identity paths

--- nix/users/flpflan/secrets.nix
+++ nix/users/flpflan/secrets.nix
@@ -3,19 +3,27 @@ let
   inherit (tools) relative;
 in
 {
-  age.secrets."users/flpflan/passwd" = {
-    file = relative "secrets/users/flpflan/passwd.age";
-  };
-  age.secrets."users/flpflan/id_ed25519" = {
-    file = relative "secrets/users/flpflan/id_ed25519.age";
-  };
-  age.secrets."users/flpflan/id_ed25519.pub" = {
-    file = relative "secrets/users/flpflan/id_ed25519.pub.age";
-  };
-  age.secrets."users/flpflan/id_rsa" = {
-    file = relative "secrets/users/flpflan/id_rsa.age";
-  };
-  age.secrets."users/flpflan/id_rsa.pub" = {
-    file = relative "secrets/users/flpflan/id_rsa.pub.age";
+  age = {
+    identityPaths = [
+      "/home/flpflan/.ssh/id_ed25519"
+      "/home/flpflan/.ssh/id_rsa"
+    ];
+    secrets = {
+      "users/flpflan/passwd" = {
+        file = relative "secrets/users/flpflan/passwd.age";
+      };
+      "users/flpflan/id_ed25519" = {
+        file = relative "secrets/users/flpflan/id_ed25519.age";
+      };
+      "users/flpflan/id_ed25519.pub" = {
+        file = relative "secrets/users/flpflan/id_ed25519.pub.age";
+      };
+      "users/flpflan/id_rsa" = {
+        file = relative "secrets/users/flpflan/id_rsa.age";
+      };
+      "users/flpflan/id_rsa.pub" = {
+        file = relative "secrets/users/flpflan/id_rsa.pub.age";
+      };
+    };
   };
 }
