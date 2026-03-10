69ec503b088676b5e66a8bc391542c3b8e473358
flpflan <flpflan@gmail.com>
2026-03-10 19:51:29 +0800

    add qt stuff to managed

--- nix/home/flpflan/fl-pc/style/manual/qt.nix
+++ nix/home/flpflan/fl-pc/style/manual/qt.nix
@@ -1,9 +1,22 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  fl-dots,
+  ...
+}:
 
+let
+  dots = "${fl-dots}/dots";
+  outOfStore = config.lib.file.mkOutOfStoreSymlink;
+in
 {
   home.packages = with pkgs; [
     qt6Packages.qt6ct
     # qt6ct-kde
     libsForQt5.qt5ct
+    # libsForQt5.qtstyleplugin-kvantum
   ];
+
+  xdg.configFile."qt5ct".source = outOfStore "${dots}/qt5ct";
+  xdg.configFile."qt6ct".source = outOfStore "${dots}/qt6ct";
 }
