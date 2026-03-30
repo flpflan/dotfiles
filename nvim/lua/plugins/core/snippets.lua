36dc5e8650951c6e8081807a8476c67b646aced3
flpflan <flpflan@gmail.com>
2026-03-30 15:30:35 +0800

    nvim: lazy_load snippets to boost startup time

--- nvim/lua/plugins/core/snippets.lua
+++ nvim/lua/plugins/core/snippets.lua
@@ -5,6 +5,6 @@ plugin("luasnip")
     updateevents = "TextChangedI, TextChangedI", -- BUG: nvim-cmp breaks with this setting
   })
   :setup(function()
-    require("luasnip.loaders.from_vscode").load()
-    require("luasnip.loaders.from_lua").load { paths = { joinpath(LUA_PATH, "snippets") } }
+    require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_lua").lazy_load { paths = { joinpath(LUA_PATH, "snippets") } }
   end)
