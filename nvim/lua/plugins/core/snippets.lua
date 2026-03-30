e66fc964ce41f3ab89ebcda9e1f6cd1fb00f6adb
flpflan <flpflan@gmail.com>
2026-03-30 15:18:46 +0800

    nvim: fix loading snippets

--- nvim/lua/plugins/core/snippets.lua
+++ nvim/lua/plugins/core/snippets.lua
@@ -5,7 +5,6 @@ plugin("luasnip")
     updateevents = "TextChangedI, TextChangedI", -- BUG: nvim-cmp breaks with this setting
   })
   :setup(function()
-    -- FIXME:
     require("luasnip.loaders.from_vscode").load()
     require("luasnip.loaders.from_lua").load { paths = { joinpath(LUA_PATH, "snippets") } }
   end)
