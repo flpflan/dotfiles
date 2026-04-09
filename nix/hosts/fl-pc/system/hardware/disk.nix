6a3943738365f2dba7a0d69d494ddec6c7bbc9cb
flpflan <flpflan@gmail.com>
2026-04-09 21:25:51 +0800

    tweak btrfs

--- nix/hosts/fl-pc/system/hardware/disk.nix
+++ nix/hosts/fl-pc/system/hardware/disk.nix
@@ -1,22 +1,38 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "weekly";
+    fileSystems = [ "/" ];
+  };
+
+  systemd.services.duperemove-home = {
+    path = [ pkgs.duperemove ];
+    script = "duperemove -r -d /home";
+    serviceConfig.Type = "oneshot";
+  };
+  systemd.timers.duperemove-home = {
+    wantedBy = [ "timers.target" ];
+    timerConfig.OnCalendar = "monthly";
+  };
+
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/3415dcb1-482f-4fea-b6aa-92b9b0c63ab0";
       fsType = "btrfs";
-      options = [ "subvol=root" "compress=zstd:3" ];
+      options = [ "subvol=root" "compress=zstd:3" "discard=async" ];
     };
 
   fileSystems."/home" =
     { device = "/dev/disk/by-uuid/3415dcb1-482f-4fea-b6aa-92b9b0c63ab0";
       fsType = "btrfs";
-      options = [ "subvol=home" "compress=zstd:3" ];
+      options = [ "subvol=home" "compress=zstd:3" "discard=async" ];
     };
 
   fileSystems."/nix" =
     { device = "/dev/disk/by-uuid/3415dcb1-482f-4fea-b6aa-92b9b0c63ab0";
       fsType = "btrfs";
-      options = [ "subvol=nix" "noatime" "compress=zstd:3" ];
+      options = [ "subvol=nix" "noatime" "compress=zstd:3" "discard=async" ];
     };
 
   fileSystems."/boot" =
