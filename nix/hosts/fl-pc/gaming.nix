b2d4c5c9c57bd7a3ab221d2b2fd4e50c7545a3df
flpflan <flpflan@gmail.com>
2026-04-10 19:58:01 +0800

    add eden switch emulator

--- nix/hosts/fl-pc/gaming.nix
+++ nix/hosts/fl-pc/gaming.nix
@@ -4,5 +4,13 @@
   #   gamescopeSession.enable = true;
   # };
   #
-  # programs.gamemode.enable = true;
+  programs.gamemode.enable = true;
+
+  # For vibration in wireless mode
+  # 此外，这可以使能承影P5手柄的飞鼠模式
+  hardware.xpadneo.enable = true;
+
+  # This is not necessary; only used to access the controller's battery level.
+  # Or use this to avoid adding user to `input` group.
+  hardware.steam-hardware.enable = true;
 }
