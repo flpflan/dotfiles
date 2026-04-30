05a15b8337a63646a00a625cd2793bd8f69e50e6
flpflan <flpflan@gmail.com>
2026-04-30 17:44:00 +0800

    remove zen browser

--- nix/home/flpflan/fl-pc/softwares/gui/browser.nix
+++ nix/home/flpflan/fl-pc/softwares/gui/browser.nix
@@ -1,6 +1,6 @@
 {tools, ...}: {
   imports = [
     (tools.relative "home/flpflan/common/apps/google-chrome")
-    (tools.relative "home/flpflan/common/apps/zen-browser")
+    # (tools.relative "home/flpflan/common/apps/zen-browser")
   ];
 }
