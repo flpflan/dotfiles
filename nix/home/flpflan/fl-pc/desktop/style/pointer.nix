aeb6ac35a1eaa4421984e22a7d1650ad9a8a2d45
flpflan <flpflan@gmail.com>
2026-06-13 17:16:11 +0800

    add Koishi cursor pointer

--- nix/home/flpflan/fl-pc/desktop/style/pointer.nix
+++ nix/home/flpflan/fl-pc/desktop/style/pointer.nix
@@ -1,5 +1,20 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.bibata-cursors ];
+  home.packages = [
+    pkgs.bibata-cursors
+    # (pkgs.runCommand "make-pointers"
+    #   {
+    #     src = fl-dots;
+    #     nativeBuildInputs = [ pkgs.unzip ];
+    #   }
+    #   ''
+    #     mkdir -p $out/share/icons
+    #     unzip $src/assets/cursor/Koishi.zip -d $out/share/icons
+    #     # for zip in $src/assets/cursor/*.zip; do
+    #     #   unzip "$zip" -d $out/share/icons
+    #     # done
+    #   ''
+    # )
+  ];
 }
